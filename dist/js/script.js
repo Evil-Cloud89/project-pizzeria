af7d948e1d130c953e2bfad3d9ca009821c14eaf
Mateusz Oko <mo89mo@gmail.com>
2023-01-11 19:09:25 +0100

    Remove class active also add accordion backbone to fill

--- dist/js/script.js
+++ dist/js/script.js
@@ -58,7 +58,9 @@
       thisProduct.id = id; 
       thisProduct.data = data; 
 
-      thisProduct.renderInMenu(); // uruchomiennie danej dunkcji funkcji po utworzeniu instancji
+      thisProduct.renderInMenu(); // uruchomiennie danej funkcji po utworzeniu instancji
+      
+      initAccordion();
 
       console.log('new Product', thisProduct);
     }
@@ -80,6 +82,27 @@
       menuContainer.appendChild(thisProduct.element);
 
     }
+
+    initAccordion(){
+      const thisProduct = this;
+  
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = ???;
+  
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+        /* prevent default action for event */
+  
+        /* find active product (product that has active class) */
+  
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+  
+        /* toggle active class on thisProduct.element */
+      });
+  
+    }
+    
+  }
 
   }
 
