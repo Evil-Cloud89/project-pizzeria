8820f74cf22c3ee80740f5fbc166a29c70c039ce
Mateusz Oko <mo89mo@gmail.com>
2023-01-23 18:37:54 +0100

    Add AmountWidget function, setValue and events

--- src/js/script.js
+++ src/js/script.js
@@ -43,8 +43,8 @@
   const settings = {
     amountWidget: {
       defaultValue: 1,
-      defaultMin: 1,
-      defaultMax: 9,
+      defaultMin: 0,
+      defaultMax: 10,
     }
   };
 
@@ -62,6 +62,7 @@
       thisProduct.getElements();
       thisProduct.initAccordion();
       thisProduct.initOrderForm();
+      thisProduct.initAmountWidget();
       thisProduct.processOrder();
 
       console.log('new Product', thisProduct);
@@ -92,7 +93,8 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
-      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper)
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
+      thisProduct.amountWidgetElem = thisProduct.element.querySelector(select.menuProduct.amountWidget);
     }
 
     initAccordion(){
@@ -203,15 +205,96 @@
           }
         }
       }
+      // multiply price by amount
+      price *= thisProduct.amountWidget.value;
+       
       // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
       console.log(price);
     }
-    
 
-  
+    initAmountWidget(){
+      const thisProduct = this;
+
+      thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
+      thisProduct.amountWidgetElem.addEventListener('updated', function(){
+        thisProduct.processOrder();
+      });
+    }
   }
+  
+  class AmountWidget{
+    constructor(element){
+      const thisWidget = this;
+
+      thisWidget.getElements(element);
+      thisWidget.setValue(settings.amountWidget.defaultValue);
+      thisWidget.initActions();
+
+      console.log('AmountWidget:', thisWidget);
+      console.log('constructor arguments:', element);
+    }
     
+    getElements(element){ // Wyszukuje elementy widgetu
+      const thisWidget = this;
+    
+      thisWidget.element = element;
+      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    }
+
+    setValue(value){ // Pośrednik - ma regulować ilość produktów i nie pozwolić na wpisywanie dowolnej ilości lub dodanie zbyt dużej lub za małej ilości produktu
+      const thisWidget = this;
+
+      const newValue = parseInt(value); // parseInt konwertuje zapis do danej liczby
+      console.log(newValue);
+
+      /* TODO: Add validation */
+      if(thisWidget.value !== newValue && !isNaN(newValue)){ 
+
+        if(newValue>=settings.amountWidget.defaultMin)
+
+          if(newValue<=settings.amountWidget.defaultMax)
+
+            thisWidget.value = newValue;
+      } 
+      
+      thisWidget.announce();
+
+      thisWidget.input.value = thisWidget.value;
+      console.log('setValue:', newValue);
+      
+    }
+
+    initActions(){
+      const thisWidget = this;
+
+      thisWidget.input.addEventListener('change', function(){
+        thisWidget.setValue(thisWidget.input.value);
+      });
+
+      thisWidget.linkDecrease.addEventListener('click', function(event){
+        event.preventDefault();
+
+        thisWidget.setValue(thisWidget.value -1);
+      });
+
+      thisWidget.linkIncrease.addEventListener('click', function(event){
+        event.preventDefault();
+
+        thisWidget.setValue(thisWidget.value +1);
+      });
+    }
+
+    announce(){
+      const thisWidget = this;
+
+      const event = new Event('updated');
+      thisWidget.element.dispatchEvent(event);
+    }
+  }
+  
   const app = {
     initMenu: function(){
       const thisApp = this;
