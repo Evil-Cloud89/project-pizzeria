1308c92a726d816d66e00b59b3034c1c067a588b
Mateusz Oko <mo89mo@gmail.com>
2023-02-04 17:02:08 +0100

    Add active subpages and fix files

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,65 @@ import Cart from './components/Cart.js';
 // import CartProduct from './components/CartProduct.js';
 
 const app = {
+  initPages: function(){
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;        // Wyszukuje wszystkie podstrony 
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);                   // Wyszukuje wszystkie linki
+    
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+        
+        /* get page id from href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        /* run thisApp.activatePage with that id */
+        thisApp.activatePage(id);
+
+        /* change URL hash */
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    /* add class "active" to matching pages, remove from non-matching */
+    for(let page of thisApp.pages){
+      // if(page.id == pageId){
+      //  page.classList.add(classNames.pages.active);
+      // } else {
+      //  page.classList.remove(classNames.pages.active);  
+      // }
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    /* add class "active" to matching links, remove from non-matching */
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+
+  },
+
   initMenu: function(){
     const thisApp = this;
     console.log('thisApp.data:', thisApp.data);
@@ -53,12 +112,13 @@ const app = {
 
   init: function(){
     const thisApp = this;
-    console.log('*** App starting ***');
-    console.log('thisApp:', thisApp);
-    console.log('classNames:', classNames);
-    console.log('settings:', settings);
-    console.log('templates:', templates);
-      
+    //console.log('*** App starting ***');
+    //console.log('thisApp:', thisApp);
+    //console.log('classNames:', classNames);
+    //console.log('settings:', settings);
+    //console.log('templates:', templates);
+
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
   },
